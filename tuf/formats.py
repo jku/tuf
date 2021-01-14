02d493ebaa7680b5ea701eb1b76799466279c40b
Jussi Kukkonen <jkukkonen@vmware.com>
2021-01-14 15:45:10 +0200

    formats: Update one more import
    Update the style to be compatible with vendoring tool.
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@vmware.com>

--- tuf/formats.py
+++ tuf/formats.py
@@ -71,7 +71,7 @@ import copy
 
 from securesystemslib import exceptions as sslib_exceptions
 from securesystemslib import formats as sslib_formats
-import securesystemslib.schema as SCHEMA
+from securesystemslib import schema as SCHEMA
 
 import tuf
 from tuf import exceptions
