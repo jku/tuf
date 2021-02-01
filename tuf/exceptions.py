12c8d9b6e2f1b957bb73e544d63bbed3eb9c0abc
Jussi Kukkonen <jkukkonen@vmware.com>
2021-02-01 14:36:27 +0200

    Add an HTTP error for Fetcher interface
    A custom error is required so that updater is able to special case
    403 & 404 status codes.
    
    Rewrite the test case a bit to be more readable.
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@vmware.com>

--- tuf/exceptions.py
+++ tuf/exceptions.py
@@ -335,3 +335,12 @@ class URLParsingError(Error):
 class InvalidConfigurationError(Error):
   """If a configuration object does not match the expected format."""
 
+class FetcherHTTPError(Exception):
+  """
+  Returned by FetcherInterface implementations for HTTP errors.
+  status_code property is the (int) HTTP status code
+  """
+  def __init__(self, message, status_code):
+    super(FetcherHTTPError, self).__init__(message)
+    self.status_code = status_code
+
